18dda79c886908c5b2926bd88300a2185663f523
Anka <runner@Mac-1748591885074.local>
2025-05-30 08:02:26 +0000

    Update formula for version v1.1.1

--- Formula/LazyKeys.rb
+++ Formula/LazyKeys.rb
@@ -1,8 +1,8 @@
 class Lazykeys < Formula
   desc "LazyKeys - Remap capslock to something useful"
   homepage "https://github.com/frostplexx/LazyKeys"
-  url "https://github.com/frostplexx/LazyKeys/releases/download/v1.1.0/lazykeys.tar.gz"
-  sha256 "7e29db73a51cb5724b4227c7d8b934afbad07d8c8ee83e273933cd873def2850"
+  url "https://github.com/frostplexx/LazyKeys/releases/download/v1.1.1/lazykeys.tar.gz"
+  sha256 "fa57c1eda1941f12ed95e31ef90f4c6681a463ab103ec9ea548cf0afa583982e"
   
   depends_on :macos
 
