ba23342ab188c2018695b8aa28dc78393aa1acbd
GitHub Action <action@github.com>
2025-08-20 15:37:02 +0000

    Update formula for version 1.3.2

--- Formula/opsops.rb
+++ Formula/opsops.rb
@@ -1,7 +1,7 @@
 class Opsops < Formula
   desc "A wrapper for sops that integrates 1Password"
   homepage "https://github.com/frostplexx/opsops"
-  version "1.3.1"
+  version "1.3.2"
 
   on_macos do
     if Hardware::CPU.arm?
